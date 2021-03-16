1c6c2c253aaf13981a003868425ff9d5bd4c1374
Jim Buchan <jbuchan42@gmail.com>
2021-03-16 18:08:15 +1300

    updated packages

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,23 @@ import "./App.css";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { apiResponse: "" }, {count: 0};
+       
     }
 
+        
+  
+    makeIncrementer = amount => () =>
+      this.setState(prevState => ({
+        count: prevState.count + amount,
+      }));
+  
+    increment = this.makeIncrementer(1);
+  
+    decrement = this.makeIncrementer(-1);  
+
+            
+    
     callAPI() {
         fetch("http://localhost:9000/testAPI")
             .then(res => res.text())
@@ -27,9 +41,13 @@ class App extends Component {
                     <h1 className="App-title">Welcome to CISE</h1>
                 </header>
                 <p className="App-intro">{this.state.apiResponse}</p>
+                <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment</button>
+        <button className="decrement" onClick={this.decrement}>Decrement</button>
             </div>
         );
     }
-}
 
+    
+    }
 export default App;
